5ac35c0c44462a1195d58ee15fbf1222783d07bb
KushagraShukla30 <1ds23cd027@dsce.edu.in>
2024-11-10 01:28:55 +0530

    Routing done

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDwQ1KOefVDYs_WoykuD29r2vP4eIDmcbE",
-    authDomain: "slack-clone-d7a12.firebaseapp.com",
-    projectId: "slack-clone-d7a12",
-    storageBucket: "slack-clone-d7a12.firebasestorage.app",
-    messagingSenderId: "987867507082",
-    appId: "1:987867507082:web:10e701342c3e2dc0dc0073",
-    measurementId: "G-TJPZQX4QZ3"
-  };
+  apiKey: "AIzaSyAq9tLGxnwRcIaMoWiCE8jNuUETvN83-4o",
+  authDomain: "peerloop01.firebaseapp.com",
+  projectId: "peerloop01",
+  storageBucket: "peerloop01.firebasestorage.app",
+  messagingSenderId: "697572645751",
+  appId: "1:697572645751:web:cae6923411b39be7741f72",
+  measurementId: "G-WE4XK2GV8H"
+};
